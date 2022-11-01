47cc69a1cc756359e9267a1f625a930e61bc9cee
Max Base <maxbasecode@gmail.com>
2022-11-01 18:39:52 +0100

    adding more test

--- main.c
+++ main.c
@@ -26,7 +26,29 @@ int main(int argc, char** argv) {
     // addBigNumbers
     BigNumber *bn3 = addBigNumbers(bn1, bn2);
     printBigNumber(bn3);
+
+    // copyBigNumber
+    BigNumber *bn4 = copyBigNumber(bn1);
+    printBigNumber(bn4);
+
+    // destroyBigNumber
+    destroyBigNumber(bn1);
+
+    // multiplyBigNumbers
+    BigNumber *bn5 = multiplyBigNumbers(bn2, bn3);
+    printBigNumber(bn5);
     
-    
+    // minusBigNumbers
+    BigNumber *bn6 = minusBigNumbers(bn5, bn4);
+    printBigNumber(bn6);
+
+    // divideBigNumbers
+    BigNumber *bn7 = divideBigNumbers(bn5, bn6);
+    printBigNumber(bn7);
+
+    // remainderBigNumbers
+    BigNumber *bn8 = remainderBigNumbers(bn5, bn6);
+    printBigNumber(bn8);
+        
     return 0;
 }
