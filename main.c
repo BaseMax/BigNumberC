58de5162b02c49d421038d67aecdf9b2eee4c4a3
Max Base <maxbasecode@gmail.com>
2022-11-01 18:26:57 +0100

    update all

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BigNumber.h"
+
+/**
+ * @brief Main function
+ * 
+ * @return int 
+ */
+int main(int argc, char** argv) {
+    // createAndSetBigNumber
+
+    
+    return 0;
+}
