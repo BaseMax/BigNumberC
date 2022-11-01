0aaa667a43f0fb9b9b7a7b54e6bcde212319e99d
Max Base <maxbasecode@gmail.com>
2022-11-01 18:23:19 +0100

    Create bignumber library

---
+++ BigNumber.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+typedef struct {
+    int *digits;
+    int size;
+} BigNumber;
+
+/**
+ * @brief Create a Big Number object
+ * 
+ * @param size 
+ * @return BigNumber* 
+ */
+BigNumber* createBigNumber(int size) {
+    BigNumber *bn = malloc(sizeof(BigNumber));
+    bn->digits = malloc(sizeof(int) * size);
+    bn->size = size;
+    return bn;
+}
+
+/**
+ * @brief Destroy the Big Number object
+ * 
+ * @param bn 
+ */
+void destroyBigNumber(BigNumber *bn) {
+    free(bn->digits);
+    free(bn);
+}
+
+/**
+ * @brief Print the Big Number object
+ * 
+ * @param bn 
+ */
+void printBigNumber(BigNumber *bn) {
+    for (int i = bn->size - 1; i >= 0; i--) {
+        printf("%d", bn->digits[i]);
+    }
+    printf("\n");
+}
+
+/**
+ * @brief Add two Big Numbers
+ * 
+ * @param bn1 
+ * @param bn2 
+ * @return BigNumber* 
+ */
+BigNumber* addBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    BigNumber *result = createBigNumber(bn1->size + 1);
+    int carry = 0;
+    for (int i = 0; i < bn1->size; i++) {
+        int sum = bn1->digits[i] + bn2->digits[i] + carry;
+        result->digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+    result->digits[bn1->size] = carry;
+    return result;
+}
+
+/**
+ * @brief Multiply two Big Numbers
+ * 
+ * @param bn1 
+ * @param bn2 
+ * @return BigNumber* 
+ */
+BigNumber* multiplyBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    BigNumber *result = createBigNumber(bn1->size + bn2->size);
+    for (int i = 0; i < bn1->size; i++) {
+        for (int j = 0; j < bn2->size; j++) {
+            result->digits[i + j] += bn1->digits[i] * bn2->digits[j];
+        }
+    }
+    for (int i = 0; i < result->size; i++) {
+        result->digits[i + 1] += result->digits[i] / 10;
+        result->digits[i] %= 10;
+    }
+    return result;
+}
+
+/**
+ * @brief Minus two Big Numbers
+ * 
+ * @param bn1
+ * @param bn2
+ * @return BigNumber*
+ */
+BigNumber* minusBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    BigNumber *result = createBigNumber(bn1->size);
+    int carry = 0;
+    for (int i = 0; i < bn1->size; i++) {
+        int sum = bn1->digits[i] - bn2->digits[i] - carry;
+        if (sum < 0) {
+            sum += 10;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        result->digits[i] = sum;
+    }
+    return result;
+}
+
+/**
+ * @brief Divide two Big Numbers
+ * 
+ * @param bn1
+ * @param bn2
+ * @return BigNumber*
+ */
+BigNumber* divideBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    BigNumber *result = createBigNumber(bn1->size);
+    BigNumber *temp = createBigNumber(bn1->size);
+    for (int i = bn1->size - 1; i >= 0; i--) {
+        for (int j = bn1->size - 1; j > i; j--) {
+            temp->digits[j] = temp->digits[j - 1];
+        }
+        temp->digits[i] = bn1->digits[i];
+        int count = 0;
+        while (1) {
+            BigNumber *temp2 = multiplyBigNumbers(bn2, createBigNumber(1));
+            if (temp2->digits[0] > temp->digits[0]) {
+                break;
+            }
+            destroyBigNumber(temp2);
+            count++;
+        }
+        result->digits[i] = count;
+        BigNumber *temp3 = multiplyBigNumbers(bn2, createBigNumber(count));
+        BigNumber *temp4 = minusBigNumbers(temp, temp3);
+        destroyBigNumber(temp3);
+        for (int j = 0; j < bn1->size; j++) {
+            temp->digits[j] = temp4->digits[j];
+        }
+        destroyBigNumber(temp4);
+    }
+    destroyBigNumber(temp);
+    return result;
+}
+
+/**
+ * @brief Remainder two Big Numbers
+ * 
+ * @param bn1
+ * @param bn2
+ * @return BigNumber*
+ */
+BigNumber* remainderBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    BigNumber *result = createBigNumber(bn1->size);
+    BigNumber *temp = createBigNumber(bn1->size);
+    for (int i = bn1->size - 1; i >= 0; i--) {
+        for (int j = bn1->size - 1; j > i; j--) {
+            temp->digits[j] = temp->digits[j - 1];
+        }
+        temp->digits[i] = bn1->digits[i];
+        int count = 0;
+        while (1) {
+            BigNumber *temp2 = multiplyBigNumbers(bn2, createBigNumber(1));
+            if (temp2->digits[0] > temp->digits[0]) {
+                break;
+            }
+            destroyBigNumber(temp2);
+            count++;
+        }
+        BigNumber *temp3 = multiplyBigNumbers(bn2, createBigNumber(count));
+        BigNumber *temp4 = minusBigNumbers(temp, temp3);
+        destroyBigNumber(temp3);
+        for (int j = 0; j < bn1->size; j++) {
+            temp->digits[j] = temp4->digits[j];
+        }
+        destroyBigNumber(temp4);
+    }
+    for (int i = 0; i < bn1->size; i++) {
+        result->digits[i] = temp->digits[i];
+    }
+    destroyBigNumber(temp);
+    return result;
+}
+
+/**
+ * @brief Compare two Big Numbers
+ * 
+ * @param bn1
+ * @param bn2
+ * @return int
+ */
+int compareBigNumbers(BigNumber *bn1, BigNumber *bn2) {
+    for (int i = bn1->size - 1; i >= 0; i--) {
+        if (bn1->digits[i] > bn2->digits[i]) {
+            return 1;
+        } else if (bn1->digits[i] < bn2->digits[i]) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * @brief Main function
+ * 
+ * @return int 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
